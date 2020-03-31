09d863a1942dfc616a0e67ad959d33b8bd1ec0e6
Mrityunjay Kumar <m.kumar1982@gmail.com>
2020-03-31 22:56:31 +0530

    Functional Interface Code
    Functional Interface Code

--- src/main/java/org/geekmj/java8/App.java
+++ src/main/java/org/geekmj/java8/App.java
@@ -1,13 +1,21 @@
 package org.geekmj.java8;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		/* Discount for Cloth is 10% of cost */
+
+		System.out.println("Selling Price of Cloth Prices 200 after discount is " + getSellingPrice(200f, 0f, cost -> (cost * 10) / 100));
+
+		/* Discount for Book is 5% of cost */
+
+		System.out.println("Selling Price of Book Priced 150 after discount is " + getSellingPrice(150f, 10f, cost -> (cost * 5) / 100));
+
+	}
+
+	private static float getSellingPrice(float cost, float couponDiscount, DiscounterFunctionalInterface discounter) {
+		return cost - couponDiscount - discounter.discount(cost);
+	}
+
 }
---
+++ src/main/java/org/geekmj/java8/DiscounterFunctionalInterface.java
@@ -0,0 +1,6 @@
+package org.geekmj.java8;
+
+@FunctionalInterface
+public interface DiscounterFunctionalInterface {
+	public float discount(float cost);
+}
